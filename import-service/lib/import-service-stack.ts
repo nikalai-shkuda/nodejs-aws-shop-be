d3fb882178e5bfb7f9fc2a47e7dbcb9db9ac7f6a
Nikalai Shkuda <nikolayshkuda@mail.ru>
2025-03-05 15:36:31 +0300

    feat(lambda): add importProductsFile handler

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,70 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import path = require("path");
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const importBucket = new s3.Bucket(this, "ImportBucket", {
+      bucketName: "my-import-aws-service-bucket",
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+        },
+      ],
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const commonLabmdaSettings = {
+      runtime: Runtime.NODEJS_22_X,
+      handler: "handler",
+      environment: {
+        BUCKET_NAME: importBucket.bucketName,
+        REGION: "eu-west-1",
+      },
+    };
+
+    const importProductsFileLambda = new NodejsFunction(
+      this,
+      "ImportProductsFileLambda",
+      {
+        ...commonLabmdaSettings,
+        entry: path.join(__dirname, "../src/handlers/importProductsFile.ts"),
+      }
+    );
+
+    const api = new apigateway.RestApi(this, "ImportServiceAPI", {
+      restApiName: "Import Service",
+      deployOptions: {
+        stageName: "dev",
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: ["OPTIONS", "GET", "PUT"],
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      },
+    });
+
+    const importResource = api.root.addResource("import");
+    importResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(importProductsFileLambda)
+    );
+
+    importBucket.grantReadWrite(importProductsFileLambda);
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+      description: "API Gateway endpoint URL for Import Service",
+    });
   }
 }
---
+++ import-service/src/handlers/importProductsFile.ts
@@ -0,0 +1,42 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from "aws-lambda";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { handleError } from "../utils/responseError";
+import { response } from "../utils/responseSuccessful";
+
+const client = new S3Client({ region: "eu-west-1" });
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    console.log(
+      "importProductsFile invoked with event:",
+      JSON.stringify(event, null, 2)
+    );
+    const fileName = event.queryStringParameters?.name;
+    if (!fileName) {
+      return handleError({
+        message: "Filename is required",
+        statusCode: 400,
+      });
+    }
+
+    const command = new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      ContentType: "text/csv",
+      Key: `uploaded/${fileName}`,
+    });
+    const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+
+    console.log(`Generated signed URL: ${signedUrl}`);
+    return response(200, signedUrl);
+  } catch (error) {
+    return handleError({ error, message: "Error generating signed URL" });
+  }
+};
