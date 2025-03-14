4e51363e20e89f19199b0a8c67df6032e0779c90
Nikalai Shkuda <nikolayshkuda@mail.ru>
2025-03-14 17:33:06 +0300

    feat(product_service): add validation in catalogBatchProcess lambda

--- product_service/src/handlers/catalogBatchProcess.ts
+++ product_service/src/handlers/catalogBatchProcess.ts
@@ -5,10 +5,16 @@ import {
   TransactWriteCommandInput,
 } from "@aws-sdk/lib-dynamodb";
 import { v4 as uuidv4 } from "uuid";
+import { ZodError } from "zod";
 import { config } from "../config";
 import { Product, Stock } from "../types/products";
 import { dynamodbClient } from "../utils/dbClient";
 import { handleError, serverErrorType } from "../utils/responseError";
+import {
+  createProductFromCsvSchema,
+  productSchema,
+  stockSchema,
+} from "../utils/schemas";
 
 const snsClient = new SNSClient({ region: config.region });
 const SNS_TOPIC_ARN = process.env.SNS_TOPIC_ARN || "";
@@ -19,17 +25,20 @@ export const handler = async (
   try {
     for (const record of event.Records) {
       const parsedProduct = JSON.parse(record.body);
+      const validatedParsedProduct =
+        createProductFromCsvSchema.parse(parsedProduct);
       const id = uuidv4();
-      const product: Product = {
+      const product: Product = productSchema.parse({
         id,
-        description: parsedProduct.description,
-        price: parsedProduct.price,
-        title: parsedProduct.title,
-      };
-      const stock: Stock = {
+        description: validatedParsedProduct.description,
+        price: Number(validatedParsedProduct.price),
+        title: validatedParsedProduct.title,
+      });
+      const stock: Stock = stockSchema.parse({
         product_id: id,
-        count: parsedProduct.count,
-      };
+        count: Number(validatedParsedProduct.count),
+      });
+
       console.log("Processing batch product:", {
         product,
         stock,
@@ -38,6 +47,16 @@ export const handler = async (
       await sendEmailNotification(product);
     }
   } catch (error) {
+    if (error instanceof ZodError) {
+      return handleError({
+        message: "Validation error",
+        error: error.errors.map((err) => ({
+          field: err.path.join("."),
+          message: err.message,
+        })),
+        statusCode: 400,
+      });
+    }
     return handleError({ error, message: "Error processing batch" });
   }
 };
